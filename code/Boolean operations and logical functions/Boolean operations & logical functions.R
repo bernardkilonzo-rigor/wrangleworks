73caa041d8582b12708eb592ed98280d17346c70
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-08-18 12:35:55 +0300

    Initial commit, boolean operators and logical functions

---
+++ code/Boolean operations and logical functions/Boolean operations & logical functions.R
@@ -0,0 +1,40 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\wrangleworks\\code\\Boolean operations and logical functions")
+#load libraries
+library(tidyverse)
+
+#load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
+#logical functions
+# CASE
+#classifying profit (share an example including resulting computations)
+
+
+
+# IF_ELSE
+
+
+# IIF
+
+
+# AND
+
+
+
+# OR
+
+
+# IFNULL
+
+
+
+# ISDATE
+
+
+# ISNULL
+
+
+# IN
+
+
+# ZN
