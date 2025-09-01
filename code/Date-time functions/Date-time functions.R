5c507dcd374fe95138d4f8a45aa9b573ec5365fd
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-01 12:48:28 +0300

    Initial commit on date-time functions in R

---
+++ code/Date-time functions/Date-time functions.R
@@ -0,0 +1,8 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\wrangleworks\\code\\Date-time functions")
+#load libraries
+library(lubridate)
+
+#load data
+superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
+#Date and time functions in R
