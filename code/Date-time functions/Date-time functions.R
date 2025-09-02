559ef2b1694123423f80932e7487db4638d02398
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2025-09-02 11:30:33 +0300

    updating date functions version 2

--- code/Date-time functions/Date-time functions.R
+++ code/Date-time functions/Date-time functions.R
@@ -4,5 +4,32 @@ library(lubridate)
 
 #load data
 superstore<-read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
-
+View(superstore)
 #Date and time functions in R
+
+#1.Converting dates from other formats
+
+#converting Order Date to a date object
+superstore$Order.Date<-dmy(superstore$Order.Date)
+
+#converting Ship Date to a date object
+superstore$Ship.Date<-as.Date(superstore$Ship.Date, format = "%d/%m/%Y")
+
+#2.Computing the current date and time
+
+#using lubridate now() function
+now()
+
+#using Sys_date or Sys_time() functions
+Sys.Date()
+Sys.time()
+
+#3 Extracting date components
+
+#extracting the year, month, and day components from the Order Date
+year(superstore$Order.Date) #extracting years
+month(superstore$Order.Date) #extracting months
+day(superstore$Order.Date) #extracting days
+
+
+
