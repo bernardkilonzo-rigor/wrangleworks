46f2ed5fede6eb8877dafcb169471c06866203ac
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-02-20 13:30:42 +0300

    exciting features with dplyr library

---
+++ code/dplyr/dplyr.R
@@ -0,0 +1,20 @@
+setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\wrangleworks\\code\\dplyr")
+
+#load dplyr library
+library(dplyr)
+
+#load data set
+superstore <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
+
+#filter_out() function
+
+
+#recode_values() function
+
+
+
+#replace_values() function
+
+
+
+#replace_when() function
