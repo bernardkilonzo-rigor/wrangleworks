9bff6322d05b9fc0679cfb43276b02c2d71d86f2
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-02-21 13:18:46 +0300

    using filter() to drop rows that don't meet certain creteria

--- code/dplyr/dplyr.R
+++ code/dplyr/dplyr.R
@@ -7,6 +7,13 @@ library(dplyr)
 superstore <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")
 
 #filter_out() function
+df <- tibble(
+  name = c("Ann","Ken","Jack", "Anita", "Alex", "Walter", "Ruth", "Lucy"),
+  Status = c("Present", "Absent", "Absent", "Present","Absent","Present","Present", "Absent"),
+  Amount = c(1000,867,1200,890,3400,1234,600,340)
+)
+#using filter() to drop records where status "present" and Amount>=1000
+df%>%filter(!(Status=="Present" & Amount>=1000))
 
 
 #recode_values() function
