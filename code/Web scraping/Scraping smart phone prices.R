42d55bd84be9adef3b2367b6494d5420269aa8a1
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-03-03 13:34:30 +0300

    extracting total reviews, logistics tags and creating sample data set.

--- code/Web scraping/Scraping smart phone prices.R
+++ code/Web scraping/Scraping smart phone prices.R
@@ -18,9 +18,17 @@ names <- page %>% html_nodes(".product-title")%>%html_text(trim = TRUE)
 prices <- page %>% html_nodes(".product-price") %>% html_text(trim = TRUE)%>%
   str_replace_all("[^0-9]", "") %>% as.numeric()
 
+reviews <- page %>% html_nodes(".reviews") %>% html_text(trim = TRUE)%>%
+  str_remove_all("[()\\[\\]]") %>% as.numeric()
+
+logistics <- page %>% html_nodes(".logistics-tag") %>% html_text(trim = TRUE)%>%
+  str_remove_all('"')
+
 #combine into a data set
 phones <- tibble(
   name = names,
-  price = prices
+  price = prices,
+  total_reviews = reviews,
+  logistics_tags = logistics
 )
 
