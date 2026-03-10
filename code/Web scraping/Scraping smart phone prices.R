e8de961af6bca1cdbc398c7e4185022e9543ae7b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-03-10 13:21:21 +0300

    scrapping multiple pages second example.

--- code/Web scraping/Scraping smart phone prices.R
+++ code/Web scraping/Scraping smart phone prices.R
@@ -100,3 +100,38 @@ phones_2<-tibble(
   rating = rating,
   total_reviews = total_reviews
 )
+
+# Extracting multiple pages example 2
+scrape_page_1 <- function(page_num){
+  url <-paste0("https://www.jumia.co.ke/catalog/?q=smartphones&page=",page_num)
+  
+  page_2 <- read_html(url)
+  
+  names <- page_2 %>% html_nodes(".name")%>%html_text(trim = TRUE)
+  
+  current_price <- page_2 %>% html_nodes(".prc") %>% html_text(trim = TRUE)%>%
+    str_replace_all("[^0-9]", "") %>% as.numeric()
+  
+  old_price <- page_2 %>% html_nodes(".old") %>% html_text(trim = TRUE)%>%
+    str_replace_all("[^0-9]", "") %>% as.numeric()
+  
+  price_deviation <- page_2 %>% html_nodes("._dsct") %>% html_text(trim = TRUE)%>%
+    str_replace_all("[^0-9]", "") %>% as.numeric()/100
+  
+  total_reviews <- page_2 %>% html_nodes(".rev") %>% html_text(trim = TRUE)%>%str_extract("(?<=\\()\\d+(?=\\))")
+  
+  rating<- page_2 %>% html_nodes(".rev") %>% html_text(trim = TRUE)%>%str_extract("\\d+\\.?\\d*")
+  
+  tibble(
+    name = names,
+    current_price_ksh = current_price,
+    old_price_ksh = old_price,
+    price_dv =price_deviation,
+    rating = rating,
+    total_reviews = total_reviews,
+    page = page_num
+  )
+}
+
+#looping through multiple pages
+scraping_5pages <-map_df(1:5, scrape_page_1)
