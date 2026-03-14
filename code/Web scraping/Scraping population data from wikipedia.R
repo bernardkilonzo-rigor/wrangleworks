e12fdec86c1eb76255156b336396f2015e144642
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-03-14 13:36:02 +0300

    dropping the column on Notes and planning next  areas to address.

--- code/Web scraping/Scraping population data from wikipedia.R
+++ code/Web scraping/Scraping population data from wikipedia.R
@@ -12,9 +12,24 @@ page <- read_html(url)
 #extract all tables
 tables <- page %>%html_table(fill = TRUE)
 length(tables) #tells how many tables are detected
+lapply(tables,head) #inspecting the tables
 
 #extract the right table (in this case the first table)
-df <- tables[[1]]
-lapply(df,head) #inspecting the table
+df <- tables[[1]]%>%select(-Notes)#drops column on Notes
+lapply(df,head) #inspecting the data
+
+#convert Population from character to numeric
+
+
+#convert %ofworld from character to percent
+
+
+#create a pipeline that automatically detects percent and converts them
+
+
+#convert date from character to a date
+
+
+#export  sample data as a CSV
 
 
