c03f2dc970da90c6caab6bd308d064ea1e94597b
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-03-14 13:13:52 +0300

    extracting population data from Wikipedia.

--- code/Web scraping/Scraping population data from wikipedia.R
+++ code/Web scraping/Scraping population data from wikipedia.R
@@ -1,12 +1,20 @@
 setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\wrangleworks\\code\\Web scraping")
 #load libraries
-library(rvest) #web scraping and HTML extraction
-library(dplyr) #data manipulation and transformation
-library(stringr) #string handling
-library(janitor) #cleaning messy data
+library(rvest) #HTML scraping
+library(dplyr) #data wrangling
+library(stringr) #text cleaning
+library(janitor) #clean column names
 
-#read the wikipedia page
+#read the Wikipedia page
 url <- "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
 page <- read_html(url)
+
+#extract all tables
+tables <- page %>%html_table(fill = TRUE)
+length(tables) #tells how many tables are detected
+
+#extract the right table (in this case the first table)
+df <- tables[[1]]
+lapply(df,head) #inspecting the table
 
 
