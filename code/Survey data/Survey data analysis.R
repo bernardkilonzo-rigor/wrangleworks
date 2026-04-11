354493552dd873fe60c786fad5f01d2064ec7c21
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-04-11 17:06:35 +0300

    exploring missing data with naniar package.

--- code/Survey data/Survey data analysis.R
+++ code/Survey data/Survey data analysis.R
@@ -6,6 +6,7 @@ library(janitor) #for cleaning names
 library(survey) #core survey analysis
 library(skimr) #for quick data summaries
 library(labelled) #for variable labels
+library(naniar) #for manipulating missing data
 
 #load data set
 survey_data <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Survey_Data_Raw.csv")%>%
@@ -14,6 +15,12 @@ survey_data <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/
 #inspecting data structure
 glimpse(survey_data)
 skim(survey_data)
+
+#explore missing data
+vis_miss(survey_data)
+miss_var_summary(survey_data)
+gg_miss_var(survey_data)
+gg_miss_case(survey_data)
 
 #cleaning and preparing variables
 #recode numeric likert values to  text labels
