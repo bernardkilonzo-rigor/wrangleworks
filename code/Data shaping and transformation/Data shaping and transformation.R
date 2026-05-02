4a26684dce0e352191efc8dd3cdbc922c4a49575
bernardkilonzo-rigor <bernardkilonzo48@gmail.com>
2026-05-02 16:12:21 +0300

    Shaping data from crosstab to long format  with pivot_longer()

--- code/Data shaping and transformation/Data shaping and transformation.R
+++ code/Data shaping and transformation/Data shaping and transformation.R
@@ -3,6 +3,18 @@ setwd("C:\\Users\\berna\\OneDrive\\Desktop\\Production\\wrangleworks\\code\\Data
 library(tidyverse)
 library(janitor)
 
-#load data set
+#load data sets
 superstore <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/main/data/Sample%20-%20Superstore.csv")%>%
   clean_names()
+Financial_data <- read.csv("https://raw.githubusercontent.com/bernardkilonzo-rigor/dataviz/refs/heads/main/data/Financial%20Data.csv", check.names = FALSE)
+
+#shaping data
+#pivot longer
+Financial_data_longpvt <- Financial_data%>%
+  pivot_longer(cols = "01/01/2024":"01/12/2025", names_to = "Date", values_to = "Values")
+
+#or
+Financial_data_log <- Financial_data%>%
+  pivot_longer(cols = contains("/"), names_to = "Date", values_to = "Value")
+
+
